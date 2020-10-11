83e11f96692fdecbb138265937d89a426dd3fe5c
DAREN <58034519+CodeFlow-Jun@users.noreply.github.com>
2020-10-11 22:08:21 +0800

    Add files via upload

---
+++ Week_04/Best_Time_to_Buy_and_Sell_Stock_II_122.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Best_Time_to_Buy_and_Sell_Stock_II_122 {
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        int len = prices.length;
+        for (int i = 0; i < len - 1; i++) {
+            int diff = prices[i + 1] - prices[i];
+            if (diff > 0) {
+                res += diff;
+            }
+        }
+        return res;
+    }
+}
---
+++ Week_04/Lemonade_Change_860.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Lemonade_Change_860 {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            switch (bill) {
+                case 5: five++; break;
+                case 10: five--; ten++; break;
+                case 20: {
+                    if (ten > 0) {
+                        ten--; five--;
+                    } else {
+                        five -= 3;
+                    }
+                    break;
+                }
+                default: break;
+            }
+            if (five < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
